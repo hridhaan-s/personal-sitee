22aea154da996e444a58cd98d126cc507319dff8
Hridhaan-Sahay <study.hridhaan@gmail.com>
2026-05-20 02:10:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -321,3 +321,24 @@ async function loadGuestbook() {
 
 loadGuestbook();
 
+
+  function showTab(tabId) {
+
+    document
+      .querySelectorAll('.tab-content')
+      .forEach(tab => {
+        tab.classList.remove('active')
+      })
+
+    document
+      .querySelectorAll('.tab-btn')
+      .forEach(btn => {
+        btn.classList.remove('active')
+      })
+
+    document
+      .getElementById(tabId)
+      .classList.add('active')
+
+    event.target.classList.add('active')
+  }
